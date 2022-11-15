2149576e4bcc997c4d7538c75cd56204e5f0f214
Ofir Berger <ofirb@vintage-ip.com>
2022-11-15 15:42:56 +0200

    first commit

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def create_table(out_df, table_name, if_exists, db_name, schema_name, idx=False)
 
 
 
-l1 = ['Active Fence', 260]
+l1 = ['Active Fence_1', 260]
 df2 = pd.DataFrame(columns = ['COMPANY_NAME', 'CURRENT_COVERAGE'])
 new_df = pd.DataFrame([l1], columns = ['COMPANY_NAME', 'CURRENT_COVERAGE'])
 df2 = pd.concat([df2,new_df], axis=0, ignore_index=True)
